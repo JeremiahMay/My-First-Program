c1db7937070c5558884db8056c9744dd2870c070
JeremiahMay <68518077+JeremiahMay@users.noreply.github.com>
2020-11-06 02:28:33 -0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -17,31 +17,29 @@ score_b = 0
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
-paddle_a.shape("square") # sets the shape of the object
-paddle_a.color("white") # sets the color of the object
+paddle_a.shape("square")
+paddle_a.color("white") 
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
-paddle_a.penup() # stops turtle from drawing a line
-paddle_a.goto(-350, 0) # sets the starting location
+paddle_a.penup() 
+paddle_a.goto(-350, 0) 
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
-paddle_b.shape("square") # sets the shape of the object
-paddle_b.color("white") # sets the color of the object
+paddle_b.shape("square")
+paddle_b.color("white") 
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
-# stretch_wid/len multiplies the object default value by 5
-# stretch_wid =5 would make width 100, len = 1, keeps it at its default
-paddle_b.penup() # stops turtle from drawing a line
-paddle_b.goto(350, 0) # sets the starting location
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("circle") # sets the shape of the object
-ball.color("white") # sets the color of the object
+ball.shape("circle")
+ball.color("white") 
 ball.shapesize(stretch_wid=1,stretch_len=1)
-ball.penup() # stops turtle from drawing a line
-ball.goto(0, 0) # sets the starting location
+ball.penup()
+ball.goto(0, 0)
 ball.dx = 3
 ball.dy = 3
 
@@ -58,9 +56,9 @@ pen.write("Player 1: 0  Player 2: 0", align= "center", font=("courier",24,"norma
 
 # Functions
 def paddle_a_up():
-    y = paddle_a.ycor() # returns the y coordinate
-    y += 20 # adds 20 pixels to y coordinate
-    paddle_a.sety(y) # .sety() sets the y coordinate to what you want
+    y = paddle_a.ycor()
+    y += 20 # 
+    paddle_a.sety(y) # 
     
 def paddle_a_down():
     y = paddle_a.ycor()
@@ -68,9 +66,9 @@ def paddle_a_down():
     paddle_a.sety(y)
     
 def paddle_b_up():
-    y = paddle_b.ycor() # returns the y coordinate
-    y += 20 # adds 20 pixels to y coordinate
-    paddle_b.sety(y) # .sety() sets the y coordinate to what you want
+    y = paddle_b.ycor()
+    y += 20 
+    paddle_b.sety(y) 
     
 def paddle_b_down():
     y = paddle_b.ycor()
@@ -78,15 +76,15 @@ def paddle_b_down():
     paddle_b.sety(y)
     
 # Keyboard Binding
-wn.listen() # tells python to listen for keyboard inputs
+wn.listen() 
 wn.onkeypress(paddle_a_up,"w")
 wn.onkeypress(paddle_a_down,"s")
-wn.onkeypress(paddle_b_up,"Up")# Up is the up arrow key
-wn.onkeypress(paddle_b_down,"Down")# Down is the down arrow key
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
 
 # main game loop
 while True:
-    wn.update() # updates screen after each loop
+    wn.update() 
     direction = [-1,1]
     
     ball.setx(ball.xcor() + ball.dx)
@@ -123,32 +121,4 @@ while True:
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40) and (ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-        
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
